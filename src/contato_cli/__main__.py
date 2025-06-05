32327868868773bbdeb4d2ddf122c3e6d7c45b9f
Arthur Ranquine <aranquine@gmail.com>
2025-06-05 08:57:16 -0300

    05-06

--- src/contato_cli/__main__.py
+++ src/contato_cli/__main__.py
@@ -1,24 +1,23 @@
+import sys
 import asyncio # biblioteca bleak requer asyncio
 import json
 import click
+from cloup import command, option
+from cloup.constraints import constraint, mutually_exclusive
 import os
-from bleak.backends.characteristic import BleakGATTCharacteristic
+import rtmidi.midiutil
+import serial
 
-from contato_cli.player import Player # classe de interação midi com o loopMIDI
+# Classes de interação MIDI com o loopMIDI
+from contato_cli.player_classes.ble_player import BLEPlayer
+from contato_cli.player_classes.com_player import COMPlayer
+# from contato_cli.player_classes.sc_player import SCPlayer
 
 from bleak import BleakClient, BleakScanner # biblioteca de BLE
 # Consultar no código embarcado
 TOUCH_CHARACTERISTIC_UUID = '62c84a29-95d6-44e4-a13d-a9372147ce21'
 GYRO_CHARACTERISTIC_UUID = '9b7580ed-9fc2-41e7-b7c2-f63de01f0692'
 ACCEL_CHARACTERISTIC_UUID = 'f62094cf-21a7-4f71-bb3f-5a5b17bb134e' 
-
-player = Player()
-def gyro_notification_handler(characteristic: BleakGATTCharacteristic, data: bytearray):
-    player.set_gyro(int.from_bytes(data, 'little', signed=True))
-def accel_notification_handler(characteristic: BleakGATTCharacteristic, data: bytearray):
-    player.set_accel(int.from_bytes(data, 'little', signed=True))
-def touch_notification_handler(characteristic: BleakGATTCharacteristic, data: bytearray):
-    player.set_touch(int.from_bytes(data, 'little', signed=False))
 
 @click.group()
 def cli() -> None:
@@ -29,6 +28,7 @@ def cli() -> None:
 def teste(texto):
     print('Texto: ' + texto)
     print(os.path.dirname(os.path.abspath(__file__)))
+
 @cli.command()
 def scan():
     asyncio.run(async_scan()) 
@@ -43,16 +43,21 @@ async def async_scan() -> None:
 #TODO: Tornar opções mutualmente exclusivas
 @click.option('--mac', '-m')
 @click.option('--dispositivo', '-d', default = 'Contato')
-
-def connect(performance, mac, dispositivo) -> None:
+@click.option('--com')
+def connect(performance, mac, dispositivo, com) -> None:
+    rtmidi.midiutil.list_output_ports()
     with open(os.path.dirname(os.path.abspath(__file__))+ '/repertorio/' + performance + '.json') as jsonfile:
         config = json.load(jsonfile)
-    player.config = config
+    if not com:
+        asyncio.run(ble_connect(config, mac, dispositivo))
+    else:
+        com_connect(config, mac, dispositivo, com)
+
+async def ble_connect(config, mac, dispositivo) -> None:
+    ble_player = BLEPlayer(config)
     print('Scan')
-    asyncio.run(async_connect(mac, dispositivo))
-async def async_connect(mac, dispositivo) -> None:
     if mac:
-        device = await BleakScanner.find_device_by_mac(mac)
+        device = await BleakScanner.find_device_by_address(mac)
     elif dispositivo:
         device = await BleakScanner.find_device_by_name(dispositivo)
     else:
@@ -63,16 +68,36 @@ async def async_connect(mac, dispositivo) -> None:
         return
 
     print("Conectando...")
-    async with BleakClient(device) as client:
+    async with BleakClient(device, disconnected_callback = lambda c : ble_player.reset_channels) as client:
         print("Conectado")
+        await client.start_notify(GYRO_CHARACTERISTIC_UUID, ble_player.set_gyro)
+        await client.start_notify(ACCEL_CHARACTERISTIC_UUID, ble_player.set_accel)
+        await client.start_notify(TOUCH_CHARACTERISTIC_UUID, ble_player.set_touch) 
+        await asyncio.sleep(3600) # COMO QUE RODA INFINITO
+        await client.stop_notify(GYRO_CHARACTERISTIC_UUID)
+        await client.stop_notify(ACCEL_CHARACTERISTIC_UUID) 
+        await client.stop_notify(TOUCH_CHARACTERISTIC_UUID)
+
+def com_connect(config, mac, dispositivo, com):
+    com_player = COMPlayer(config)
+    serial_port = serial.Serial(port = 'COM' + com, 
+                                baudrate=115200,
+                                timeout=2, 
+                                stopbits=serial.STOPBITS_ONE)
+    try:
         while True:
-            await client.start_notify(GYRO_CHARACTERISTIC_UUID, gyro_notification_handler)
-            await client.start_notify(TOUCH_CHARACTERISTIC_UUID, touch_notification_handler) 
-            await client.start_notify(ACCEL_CHARACTERISTIC_UUID, accel_notification_handler)
-            await asyncio.sleep(3600) # COMO QUE RODA INFINITO
-            await client.stop_notify(GYRO_CHARACTERISTIC_UUID)
-            await client.stop_notify(TOUCH_CHARACTERISTIC_UUID)
-            await client.stop_notify(ACCEL_CHARACTERISTIC_UUID) 
+            if(serial_port.in_waiting > 0):
+                serial_string = serial_port.readline()
+                sensor_data_list = (serial_string.decode('utf-8')).split('/')
+                id = int(sensor_data_list[0])
+                com_player.set_gyro(int(sensor_data_list[1])) #TODO: Ver se gyro pode ser um int
+                com_player.set_accel(float(sensor_data_list[2]))
+                com_player.set_touch(int(sensor_data_list[3]))
+
+                # Output
+                print(f'{id} gyro: {com_player.gyro} acc: {com_player.accel} t: {com_player.touch}') 
+    except:
+        com_player.reset_channels()
 
 if __name__ == "__main__":
     cli()
---
+++ src/contato_cli/player_classes/ble_player.py
@@ -0,0 +1,93 @@
+import rtmidi
+import time
+from contato_cli.player_classes.player import Player
+from bleak.backends.characteristic import BleakGATTCharacteristic
+
+class BLEPlayer(Player):
+    def __init__(self, config):
+        super().__init__(config)
+    
+    def play_notes(self, device, note_codes_list) -> None:
+        for note_code in note_codes_list: # [36, 40, 43] 
+            match device:
+                case 'gyro':
+                    if self.pianissimo_flag:
+                        self.gyro_midiout.send_message([143 + self.config.get('midiout_port'), 
+                                                note_code, # 36
+                                                127])
+                        print(f'[Gyro] On (pianissimo): {note_codes_list}')
+                    else:
+                        self.gyro_midiout.send_message([143 + self.config.get('midiout_port'), 
+                                    note_code, # 36
+                                    127])
+                        print(f'[Gyro] On: {note_codes_list}')
+                    self.last_gyro_notes_played_list = note_codes_list
+                case 'accel':
+                    self.accel_midiout.send_message([143 + self.config.get('midiout_port'), 
+                                    note_code, # 36
+                                    100])
+                    print(f'[Accel] On: {note_codes_list}')
+    
+    def stop_notes(self, device, note_codes_list) -> None:
+        for note_code in note_codes_list: # [36, 40, 43]    
+            match device:
+                case 'gyro':
+                    self.gyro_midiout.send_message([127 + self.config.get('midiout_port'), 
+                                        note_code, # 36
+                                        100])
+                    self.last_gyro_notes_played_list = note_codes_list
+                    print(f'[Gyro] off: {note_codes_list}')
+                case 'accel':
+                    self.accel_midiout.send_message([127 + self.config.get('midiout_port'), 
+                                        note_code, # 36
+                                        100])     
+                    print(f'[Accel] off: {note_codes_list}')   
+
+    def set_gyro(self, characteristic: BleakGATTCharacteristic, data: bytearray) -> None:
+        self.gyro = int.from_bytes(data, 'little', signed=True) * self.config.get('hand')
+        # Notas atuais
+        for notes in self.config.get('angle_notes_list'): # [0, [['C', 3], ['E', 3], ['G', 3]]]
+            notes_list = notes[1] 
+            if self.gyro <= notes[0]: # TODO: testar limite infinito das notas
+                break
+
+        self.current_gyro_notes = self.convert_to_midi_codes(notes_list) 
+        # current_gyro_notes == [36, 40, 43]
+
+    def set_accel(self, characteristic: BleakGATTCharacteristic, data: bytearray) -> None:
+        self.accel = int.from_bytes(data, 'little', signed=True)
+        if time.time() - self.last_accel_trigger_time > self.config.get('accel_delay'):
+            if abs(self.accel) > self.config.get('accel_sensitivity_+') or abs(self.accel) > self.config.get('accel_sensitivity_-'):
+                if self.config.get('legato'): # Caso legato esteja ativado, a funcionalidade será interromper última nota
+                    self.stop_notes('gyro', self.last_gyro_notes_played_list)     
+                self.play_notes('accel', self.convert_to_midi_codes(self.config.get('accel_notes')))
+                self.last_accel_trigger_time = time.time()
+                self.accel_flag = True
+            
+            elif self.accel_flag:
+                self.stop_notes('accel', self.convert_to_midi_codes(self.config.get('accel_notes')))
+                self.accel_flag = False    
+
+    def set_touch(self, characteristic: BleakGATTCharacteristic, data: bytearray) -> None:
+        self.touch = int.from_bytes(data, 'little', signed=False)
+        if self.touch: 
+            # Início do toque
+            if not self.touch_flag:
+                self.stop_notes('gyro', self.last_gyro_notes_played_list)
+                if self.touch == 2:
+                    self.pianissimo_flag = True
+                else:
+                    self.pianissimo_flag = False
+                self.play_notes('gyro', self.current_gyro_notes)
+                self.touch_flag = True 
+    
+            # Decorrer do toque
+            if self.current_gyro_notes != self.last_gyro_notes_played_list:
+                self.stop_notes('gyro', self.last_gyro_notes_played_list)
+                self.play_notes('gyro', self.current_gyro_notes)
+        else:
+            # Liberação do toque
+            if self.touch_flag:
+                if not self.config.get('legato'): 
+                    self.stop_notes('gyro', self.last_gyro_notes_played_list)
+                self.touch_flag = False
